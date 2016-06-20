bbfb6e5f23cf42b54fec6a02074803c7e8f3a197
Ran Wahle <ran.wahle@gmail.com>
2016-06-20 15:15:52 +0300

    async await

--- public/8.Iterator-RealWorld/IteratorRealWorld.js
+++ public/8.Iterator-RealWorld/IteratorRealWorld.js
@@ -1,46 +1,53 @@
 /**
  * Created by Ran.Wahle on 6/20/2016.
  */
-function* getUserAndChildren(){
+function* getUserAndChildren() {
     let users = yield getUsers();
 
-    let children = yield getFirstUserChildren();
+     yield getFirstUserChildren();
 
 }
 
-function getUsers(){
-    return  fetch( '/users').then(response=> response.json());
+function getUsers() {
+    return fetch('/users').then(response=> response.json());
 
 }
 
-function getFirstUserChildren(){
+function getFirstUserChildren() {
     return fetch('/users/1/children').then(response => response.json());
 }
 document.addEventListener('DOMContentLoaded', () => {
     let usersAndChildren = getUserAndChildren();
 
-    let promises =[];
+    let promises = [];
     let current = usersAndChildren.next();
 
     do {
         console.log(current.value);
         promises.push(current);
-         current = usersAndChildren.next();
+        current = usersAndChildren.next();
 
 
-    }while(!current.done);
+    } while (!current.done);
 
-    Promise.all(promises).then(responses => console.log(responses));
-    // let users = Promise.resolve( usersAndChildren.next().value);
-    // users.forEach(user => {
-    //     document.getElementById('parent').innerHTML += 'Name: ' + user.name + ' Last Name: ' + user.lastName + '</br>';
-    // });
-    //
-    // // while (usersAndChildren.next())
-    // // {
-    // //
-    // // };
-    console.log(usersAndChildren);
+    Promise.all(promises).then(promises =>
+        promises.forEach(promise => {
+            console.log('promise', promise);
+            promise.value.then(response => console.log(response));
+        })
+    );
+    
+// let users = Promise.resolve( usersAndChildren.next().value);
+// users.forEach(user => {
+//     document.getElementById('parent').innerHTML += 'Name: ' + user.name + ' Last Name: ' + user.lastName + '</br>';
+// });
+//
+// // while (usersAndChildren.next())
+// // {
+// //
+// // };
+console.log(usersAndChildren);
 
 
-});
+})
+;
